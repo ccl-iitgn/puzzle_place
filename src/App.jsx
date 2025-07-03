a60e518157d4cff5f497eee4542bdcd374fff6ea
bhanupattemz <bhanupattemz@gmail.com>
2025-07-03 20:40:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <main>
+      <h1>Puzzle Place</h1>
+      <div>
+        <a href="https://chess-avoid-puzzle.vercel.app/" target="_blank">Chess Avoiding Puzzle</a>
+        <a href="https://upitup-ten.vercel.app/" target="_blank">Upitup Puzzle</a>
+        <a href="https://jumpingjulia.vercel.app/" target="_blank">Jumping Julia</a>
+        <a href="https://paperfolding.vercel.app/" target="_blank">Paper Folding Puzzle</a>
+        <a href="https://norightturn.vercel.app/" target="_blank">No Right Turn Maze</a>
+        <a href="https://fourcolors-mu.vercel.app/" target="_blank">Four Colors Matching Puzzle</a>
+        <a href="https://hexagon-edge-matching.vercel.app/" target="_blank">Hexagon Edge Matching Puzzle</a>
+        <a href="https://edge-matching-3d.vercel.app/" target="_blank">Cube Edge Matching Puzzle</a>
+        <a href="https://mathmosaic.vercel.app/" target="_blank">Square Matching Puzzle</a>
+      </div>
+    </main>
+  )
+}
+
+export default App
