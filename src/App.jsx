88204eb49da5850e6a798ead1a22a4370467be8f
Centre for Creative Learning IITGN <107262222+ccl-iitgn@users.noreply.github.com>
2025-07-09 15:18:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ function App() {
   return (
     <main>
       <h1>Puzzle Place</h1>
+     <h2>Videos</h2>
+      <div>
+        <a href="https://www.youtube.com/watch?v=h0rQIdfrweI" target="_blank">Skyscraper Puzzle</a>
+        <a href="https://www.youtube.com/watch?v=fiZQbsnFtOE">Polyomino Alphabet Fitting Puzzle</a>
+      </div>
+     <h2>Web Pages</h2>
       <div>
         <a href="https://chess-avoid-puzzle.vercel.app/" target="_blank">Chess Avoiding Puzzle</a>
         <a href="https://upitup-ten.vercel.app/" target="_blank">Upitup Puzzle</a>
@@ -18,6 +24,7 @@ function App() {
         <a href="https://edge-matching-3d.vercel.app/" target="_blank">Cube Edge Matching Puzzle</a>
         <a href="https://mathmosaic.vercel.app/" target="_blank">Square Matching Puzzle</a>
       </div>
+     
     </main>
   )
 }
